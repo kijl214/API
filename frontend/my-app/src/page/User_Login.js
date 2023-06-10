205a29414dd0804ee0df3af6d49f92d21b8c3634
kijl214 <wongkinman214@gmail.com>
2023-06-10 16:04:38 +0800

    Add new page user login, sign up.

---
+++ frontend/my-app/src/page/User_Login.js
@@ -0,0 +1,108 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+
+const Login = ({userhandleLogin}) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [rememberMe, setRememberMe] = useState(false);
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleRememberMeChange = (event) => {
+    setRememberMe(event.target.checked);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      // Send a POST request to the server to log in the user
+      const response = await axios.post('http://localhost:3001/user/login', {
+        username,
+        password
+      });
+
+      // If the login is successful, save the token and user data to local storage
+      localStorage.setItem('token', response.data.token);
+      localStorage.setItem('user', JSON.stringify(response.data.user));
+
+      // If the "remember me" checkbox is checked, save the token and user data to session storage
+      if (rememberMe) {
+        sessionStorage.setItem('token', response.data.token);
+        sessionStorage.setItem('user', JSON.stringify(response.data.user));
+      }
+      userhandleLogin();
+      // Navigate the user to the home page
+      navigate('/create-cat');
+    } catch (error) {
+      // If there's an error, set the error state to display the error message to the user
+      setError(error.response.data.error);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h3>User Login</h3>
+      {error && (
+        <div className="alert alert-danger" role="alert">
+          {error}
+        </div>
+      )}
+      <div className="mb-3">
+        <label htmlFor="username">Username</label>
+        <input
+          type="text"
+          className="form-control"
+          id="username"
+          value={username}
+          onChange={handleUsernameChange}
+          placeholder="Enter username"
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          className="form-control"
+          id="password"
+          value={password}
+          onChange={handlePasswordChange}
+          placeholder="Enter password"
+        />
+      </div>
+      <div className="mb-3">
+        <div className="custom-control custom-checkbox">
+          <input
+            type="checkbox"
+            className="custom-control-input"
+            id="rememberMe"
+            checked={rememberMe}
+            onChange={handleRememberMeChange}
+          />
+          <label className="custom-control-label" htmlFor="rememberMe">
+            Remember me
+          </label>
+        </div>
+      </div>
+      <div className="d-grid">
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </div>
+      <p className="forgot-password text-right">
+        Forgot <a href="/forgot-password">password?</a>
+      </p>
+    </form>
+  );
+};
+
+export default Login;
---
+++ frontend/my-app/src/page/User_SignUp.js
@@ -0,0 +1,100 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+
+const SignUp = () => {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    // Make sure the passwords match
+    if (password !== confirmPassword) {
+      setError('Passwords do not match');
+      return;
+    }
+
+    // Call the API to sign up the staff member with the username, email, password
+    try {
+      await axios.post('http://localhost:3001/user/signup', {
+        username,
+        email,
+        password,
+      });
+
+      navigate('/user-sign-in');
+    } catch (error) {
+      setError(error.response.data.error);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h3>User Sign Up</h3>
+      {error && (
+        <div className="alert alert-danger" role="alert">
+          {error}
+        </div>
+      )}
+      <div className="mb-3">
+        <label htmlFor="username">Username</label>
+        <input
+          type="text"
+          className="form-control"
+          id="username"
+          value={username}
+          onChange={(event) => setUsername(event.target.value)}
+          placeholder="Username"
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="email">Email address</label>
+        <input
+          type="email"
+          className="form-control"
+          id="email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+          placeholder="Enter email"
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          className="form-control"
+          id="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+          placeholder="Enter password"
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="confirmPassword">Confirm Password</label>
+        <input
+          type="password"
+          className="form-control"
+          id="confirmPassword"
+          value={confirmPassword}
+          onChange={(event) => setConfirmPassword(event.target.value)}
+          placeholder="Confirm password"
+        />
+      </div>
+      <div className="d-grid">
+        <button type="submit" className="btn btn-primary">
+          Sign Up
+        </button>
+      </div>
+      <p className="forgot-password text-right">
+        Already registered <a href="/sign-in">sign in?</a>
+      </p>
+    </form>
+  );
+};
+
+export default SignUp;
